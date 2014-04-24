c5df8d3162fdb2ab59840b1af5a85bfb91249942
Firede <firede@firede.us>
2014-04-24 11:25:50 +0800

    init repo.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
-}).listen(port);
+
+app.get('/', function (req, res) {
+    res.format({
+        html: function () {
+            res.send('<h1>Hello, World!</h1>');
+        },
+        json: function () {
+            res.send({ content: '<h1>Hello World!</h1>'});
+        }
+    });
+
+});
+
+app.listen(port);
