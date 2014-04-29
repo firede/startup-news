04a6e9400485193138f43acff9c7a7088e3e9411
Firede <firede@firede.us>
2014-04-29 17:13:12 +0800

    updated static files.

--- public/asset/list/hot.tpl.js
+++ public/asset/list/hot.tpl.js
@@ -1,2 +1,1 @@
-/*! 2014 Baidu Inc. All Rights Reserved */
-define("list/hot.tpl",function(){return'<!-- use: mainHeader(pageName=\'list_hot\', showNav=true) -->\n<div class="news-container">\n\n    <div class="news-list">\n        <!-- import: newsList -->\n    </div>\n\n    <!-- if: ${more} -->\n    <a class="news-list-more">加载更多</a>\n    <!-- /if -->\n\n</div>\n'});
+define(function () {return '<!-- use: mainHeader(pageName=\'list_hot\', showNav=true) -->\n<div class="news-container">\n\n    <div class="news-list">\n        <!-- import: newsList -->\n    </div>\n\n    <!-- if: ${more} -->\n    <a class="news-list-more">加载更多</a>\n    <!-- /if -->\n\n</div>\n';});
